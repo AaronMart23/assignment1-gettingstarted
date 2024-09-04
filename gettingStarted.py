2cd27aed99ec0e37c720aa6b888d507cdb0f33e6
AaronMart23 <am12374@nyu.edu>
2024-09-03 20:36:10 -0400

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -4,17 +4,28 @@
 
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
+    #Another way to implement is using a "case" statements similar to C
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "bec78115f2723e6d4f5643f24d2e8d88ca4bc75a1c1d58e12fe4f0186a381805"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "3"  # DNS belongs to the Application layer in the OSI model, but the Transport layer in TCP/IP model
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "3"  # ICMP belongs to the Network layer in the TCP/IP model
+    else:
+        # If the question doesn't match any of the known questions, this will catch it
         answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    return(answer)
+    return answer
+
 # Complete all the questions.
 
 
